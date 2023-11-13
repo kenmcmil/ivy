ab5ae7830571c7fa2668d3d8cc4d7c72c29abfe6
George Pîrlea <george@pirlea.net>
2023-11-13 17:50:37 +0200

    Refactor

--- ivy/ivy_mypyvy.py
+++ ivy/ivy_mypyvy.py
@@ -2,10 +2,12 @@
 # Copyright (c) Microsoft Corporation. All Rights Reserved.
 #
 
-from . import ivy_module as im
+from . import ivy_ast as ia
 from . import ivy_logic as il
-from . import ivy_isolate
-from . import ivy_compiler
+from . import ivy_logic_utils as ilu
+from . import ivy_module as im
+from . import ivy_trace as it
+from . import logic as lg
 from . import mypyvy_syntax as pyv
 
 logfile = None
@@ -14,7 +16,35 @@ verbose = False
 # Ivy symbols have dots in them (due to the module system)
 # but mypyvy doesn't allow dots in names, so we replace
 # them with this string
-DOT_REPLACEMENT = "__"
+DOT_REPLACEMENT = "_"
+
+class Translation:
+    '''Helper class for translating Ivy expressions to mypyvy expressions.'''
+    def sort_type(ivy_sort):
+        if il.is_function_sort(ivy_sort) and il.is_boolean_sort(ivy_sort.rng):
+            return 'relation'
+        elif il.is_function_sort(ivy_sort):
+            return 'function'
+        return 'individual'
+
+    def to_pyv_sort(ivy_sort):
+        if il.is_first_order_sort(ivy_sort) \
+            or il.is_enumerated_sort(ivy_sort):
+            return pyv.UninterpretedSort(ivy_sort.name)
+        elif il.is_boolean_sort(ivy_sort):
+            return pyv.BoolSort
+        # Relation
+        elif il.is_function_sort(ivy_sort) and il.is_boolean_sort(ivy_sort.rng):
+            # FIXME: do we need the bool for rng?
+            return tuple([Translation.to_pyv_sort(x) for x in ivy_sort.dom])
+        elif il.is_function_sort(ivy_sort):
+            return (tuple([Translation.to_pyv_sort(x) for x in ivy_sort.dom]), Translation.to_pyv_sort(ivy_sort.rng))
+        else:
+            raise NotImplementedError("translating sort {} to mypyvy ".format(repr(ivy_sort)))
+
+    def to_pyv_name(ivy_name):
+        return ivy_name.replace(".", DOT_REPLACEMENT)
+
 
 # Our own class, which will be used to generate a mypyvy `Program`
 class MypyvyProgram:
@@ -29,32 +59,6 @@ class MypyvyProgram:
         self.functions = []
         self.axioms = []
 
-    def sort_type(self, ivy_sort):
-        if il.is_function_sort(ivy_sort) and il.is_boolean_sort(ivy_sort.rng):
-            return 'relation'
-        elif il.is_function_sort(ivy_sort):
-            return 'function'
-        return 'individual'
-
-    def to_pyv_sort(self, ivy_sort):
-        if il.is_first_order_sort(ivy_sort) \
-            or il.is_enumerated_sort(ivy_sort):
-            return pyv.UninterpretedSort(ivy_sort.name)
-        elif il.is_boolean_sort(ivy_sort):
-            return pyv.BoolSort
-        # Relation
-        elif il.is_function_sort(ivy_sort) and il.is_boolean_sort(ivy_sort.rng):
-            # FIXME: do we need the bool for rng?
-            return tuple([self.to_pyv_sort(x) for x in ivy_sort.dom])
-        elif il.is_function_sort(ivy_sort):
-            return (tuple([self.to_pyv_sort(x) for x in ivy_sort.dom]), self.to_pyv_sort(ivy_sort.rng))
-        else:
-            raise NotImplementedError("translating sort {} to mypyvy ".format(repr(ivy_sort)))
-
-    def to_pyv_name(self, ivy_name):
-        return ivy_name.replace(".", DOT_REPLACEMENT)
-
-
     def add_constant_if_not_exists(self, cst):
         if cst.name not in [x.name for x in self.constants]:
             self.constants.append(cst)
@@ -67,7 +71,7 @@ class MypyvyProgram:
         elif il.is_enumerated_sort(sort):
             # Declare the sort
             decl = pyv.SortDecl(sort.name)
-            pyv_sort = self.to_pyv_sort(sort)
+            pyv_sort = Translation.to_pyv_sort(sort)
             self.sorts.append(decl)
 
             # Add constants (individuals) for each enum value
@@ -89,7 +93,9 @@ class MypyvyProgram:
             # print("unhandled sort: {}".format(sort))
             raise NotImplementedError("sort {} not supported".format(sort))
 
-    def translate_ivy_sig(self, sig):
+    def translate_ivy_sig(self, sig: il.Sig):
+        '''Translate a module signature to the sorts, constants,
+        relations, and functions of a mypyvy specification.'''
         # Add sorts
         for (sort_name, sort) in sig.sorts.items():
             self.add_sort(sort)
@@ -98,29 +104,29 @@ class MypyvyProgram:
         for _sym_name, sym in sig.symbols.items():
             assert _sym_name == sym.name, "symbol name mismatch: {} != {}".format(_sym_name, sym.name)
             sort = sym.sort
-            kind = self.sort_type(sort)
-            name = self.to_pyv_name(sym.name)
+            kind = Translation.sort_type(sort)
+            name = Translation.to_pyv_name(sym.name)
 
             # FIXME: how to determine if (im)mutable?
             # Ivy has no such distinction.
             is_mutable = True
 
             if kind == 'individual':
-                pyv_sort = self.to_pyv_sort(sort)
+                pyv_sort = Translation.to_pyv_sort(sort)
                 const = pyv.ConstantDecl(name, pyv_sort, is_mutable)
                 self.add_constant_if_not_exists(const)
             elif kind == 'relation':
                 assert sym.is_relation()
-                pyv_sort = self.to_pyv_sort(sort)
+                pyv_sort = Translation.to_pyv_sort(sort)
                 # assert isinstance(pyv_sort, tuple)
                 rel = pyv.RelationDecl(name, pyv_sort, is_mutable)
                 self.relations.append(rel)
             elif kind == 'function':
-                (dom_sort, rng_sort) = self.to_pyv_sort(sort)
+                (dom_sort, rng_sort) = Translation.to_pyv_sort(sort)
                 fn = pyv.FunctionDecl(name, dom_sort, rng_sort, is_mutable)
                 self.functions.append(fn)
-
-    def to_program(self):
+ 
+    def to_program(self) -> pyv.Program:
         decls = self.sorts + self.constants + self.relations + self.functions + self.axioms
         return pyv.Program(decls)
 
@@ -129,26 +135,38 @@ def check_isolate():
     mod = im.module
     prog = MypyvyProgram()
 
-    # mod.sort_order -> sorts
-    # mod.functions -> all relations, constants, and functions?
-    # mod.labeled_props -> properties/conjectures/invariants
-    # mod.initializers -> after init
-    # mod.public_actions
-    # mod.actions
-    # mode.sig = sig
-    #   sig.sorts
-    #   sig.symbols (and sig.all_symbols())
-    #   sig.constructors
-    #   sig.interp
-    # take a look at sig_to_str()
-    # mod.isolates
-
-    # Drop into a REPL by typing interact()
-    import pdb;pdb.set_trace()
+    # FIXME: do we need to handle mod.aliases? (type aliases)
 
     # STEP 1: parse mod.sig to determine
     # sorts, relations, functions, and individuals
+    # mod.sig = sig
+    #   sig.sorts
+    #   sig.symbols (and sig.all_symbols())
     prog.translate_ivy_sig(mod.sig)
+
+    # STEP 2: add axioms and conjectures
+    # Drop into a REPL by typing interact()
+    # mod.axioms
+    # mod.labeled_props -> properties (become axioms once checked)
+    # mod.labeled_conjs -> invariants/conjectures (to be checked)
+
+    import pdb;pdb.set_trace()
+
+    # STEP 3: generate actions
+    # - collect all implicitly existentially quantified variables (those starting with __)
+    # mod.initializers -> after init
+    # mod.public_actions
+    # mod.actions
+
+    # Generate VCs:
+    # _true = lg.And()
+    # _false = lg.Or()
+    # it.make_vc(action, _true, _false)
+
+    # What's used in ivy_vmt.py seems easier to work with
+    # upd = a.update(im.module,None)
+    # upd[0] describes the referenced symbols? (or modified symbols?)
+    # upd[1] describes the formula
 
     #  Generate the program
     pyv_prog = prog.to_program()
